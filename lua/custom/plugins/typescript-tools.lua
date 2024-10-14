d17c49052294cf46de0ff2187d130d48a38a6634
Nick van Dyke <nickjvandyke15@gmail.com>
2024-10-14 15:38:25 -0600

    add TSServer code actions and closing tags

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -3,6 +3,11 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   opts = {
     settings = {
+      expose_as_code_action = 'all',
+      jsx_close_tag = {
+        enable = true,
+        filetypes = { 'typescriptreact', 'javascriptreact' },
+      },
       tsserver_format_options = {
         convertTabsToSpaces = false,
       },
