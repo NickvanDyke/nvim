c7bf5f1b61e3190c1ff82bc1aa884ceb41930d05
Nick van Dyke <nickjvandyke15@gmail.com>
2024-10-16 10:29:05 -0600

    Prefer prettier for TS formatting

--- init.lua
+++ init.lua
@@ -726,6 +726,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', lsp_format = 'fallback', stop_after_first = true },
       },
     },
   },
