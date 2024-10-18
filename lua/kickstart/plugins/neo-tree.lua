1ac06a73ec0c0fc0a5443b94ec0ac58fc5c6cae0
Nick van Dyke <nickjvandyke15@gmail.com>
2024-10-18 17:15:58 -0600

    Auto close neotree after open file

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -28,5 +28,16 @@ return {
         leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
       },
     },
+    event_handlers = {
+      {
+        event = 'file_open_requested',
+        handler = function()
+          -- auto close
+          -- vim.cmd("Neotree close")
+          -- OR
+          require('neo-tree.command').execute { action = 'close' }
+        end,
+      },
+    },
   },
 }
