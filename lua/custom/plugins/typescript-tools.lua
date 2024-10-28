af4fb95f93574fe47f656c7deb08bc6446e1b996
Nick van Dyke <nickjvandyke15@gmail.com>
2024-10-28 14:33:45 -0600

    Disable diagnostic tsserver, too much memory

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -3,6 +3,7 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   opts = {
     settings = {
+      separate_diagnostic_server = false, -- Uses too much memory in Polco
       expose_as_code_action = 'all',
       jsx_close_tag = {
         enable = true,
