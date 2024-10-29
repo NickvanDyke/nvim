afa2e2772ff7fbf42fdd7aee648aa65901e30453
Nick van Dyke <nickjvandyke15@gmail.com>
2024-10-28 19:31:40 -0600

    Add LSP incoming/outgoing calls

--- init.lua
+++ init.lua
@@ -484,6 +484,8 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<leader>chi', vim.lsp.buf.incoming_calls, '[C]ode [H]ierarchy [I]ncoming')
+          map('<leader>cho', vim.lsp.buf.outgoing_calls, '[C]ode [H]ierarchy [O]utgoing')
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
