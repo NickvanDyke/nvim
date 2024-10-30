2516993b0f1ee817483fb4e755615799c406741c
Nick van Dyke <nickjvandyke15@gmail.com>
2024-10-30 09:19:40 -0600

    Sync smoothcursor color with mini.statusline mode color

--- lua/custom/plugins/smoothcursor.lua
+++ lua/custom/plugins/smoothcursor.lua
@@ -3,8 +3,24 @@ return {
   config = function()
     require('smoothcursor').setup {
       cursor = '',
-      -- linehl = 'CursorLine',
       speed = 17,
+      disable_float_win = true, -- looks bad in Arrow
     }
+
+    function sync_cursor_to_mini_mode()
+      local mode, mode_hl = require('mini.statusline').section_mode {}
+      local hl = vim.api.nvim_get_hl_by_name(mode_hl, true)
+
+      local background = hl.background and string.format('#%06x', hl.background) or nil
+
+      vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = background })
+    end
+
+    sync_cursor_to_mini_mode()
+
+    -- Sync cursor color with mini.statusline mode colors
+    vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
+      callback = sync_cursor_to_mini_mode,
+    })
   end,
 }
