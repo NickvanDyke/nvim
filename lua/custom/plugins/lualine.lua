6f4cfd94c1981ec068a440a2f0431a6333ef7349
Nick van Dyke <nickjvandyke15@gmail.com>
2024-10-30 16:27:18 -0600

    Rounded lualine :D

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,14 +2,21 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local lualine = require('lualine')
+    local lualine = require 'lualine'
 
     lualine.setup {
+      options = {
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+      },
       sections = {
         lualine_c = {
           {
             'filename',
             path = 1,
+            fmt = function(str)
+              return string.gsub(str, '/', ' > ')
+            end,
           },
         },
         lualine_x = { 'filetype' },
