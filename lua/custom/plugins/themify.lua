db6bedb73b3fbd97d17f923ef4c844eedc44357e
Nick van Dyke <nickjvandyke15@gmail.com>
2024-10-30 15:05:36 -0600

    Fix signcolumn color in gruvbox

--- lua/custom/plugins/themify.lua
+++ lua/custom/plugins/themify.lua
@@ -8,7 +8,16 @@ return {
     require('themify').setup {
       -- 'Tsuzat/NeoSolarized.nvim',
       'folke/tokyonight.nvim',
-      'ellisonleao/gruvbox.nvim',
+      {
+        'ellisonleao/gruvbox.nvim',
+        before = function()
+          require('gruvbox').setup {
+            overrides = {
+              SignColumn = { bg = 'none' },
+            },
+          }
+        end,
+      },
       'projekt0n/github-nvim-theme',
       'catppuccin/nvim',
       'default',
