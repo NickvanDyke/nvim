01de19f897119ca184fc45090e52d38d9563ddb7
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-01 13:44:39 -0600

    Always integrate flash with search instead of separate keybind/function

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -2,13 +2,32 @@ return {
   'folke/flash.nvim',
   event = 'VeryLazy',
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    modes = {
+      search = {
+        -- Just always integrate with search
+        -- instead of having an entirely separate function.
+        -- Mainly to keep s free for surround plugin.
+        -- Can always just press enter to finish search normally;
+        -- Seems like a win-win.
+        enabled = true,
+        highlight = {
+          backdrop = true,
+        }
+      }
+    }
+  },
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+
+    { "R", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    -- Not really sure what this does over just treesitter()?
+    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
   },
 }
