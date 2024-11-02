2be056879a68eb2f5adf59661b4c2de9e1cf156a
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-01 19:37:52 -0600

    Auto-reload file when changing in git

--- init.lua
+++ init.lua
@@ -258,6 +258,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('CursorHold', {
+  desc = 'Trigger autoread (in case file changed externally)',
+  callback = function()
+    vim.cmd 'checktime'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
