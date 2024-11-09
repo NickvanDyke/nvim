503aa9a1489f1ef8a32494892749c2288bde2556
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-10 06:51:10 +0900

    Oops I pasted default snack term options

--- lua/nvandyke/plugins/snacks.lua
+++ lua/nvandyke/plugins/snacks.lua
@@ -6,46 +6,18 @@ return {
     bigfile = { enabled = true },
     terminal = {
       enabled = true,
+      win = {
+        wo = {
+          -- TODO: how to disable this? Don't need with prompt
+          winbar = nil,
+        },
+      },
     },
     lazygit = { enabled = true },
     words = { enabled = true },
     statuscolumn = { enabled = false }, -- tf, is everything enabled by default?
     notifier = { enabled = false },
-    styles = {
-      terminal = {
-        bo = {
-          filetype = 'snacks_terminal',
-        },
-        wo = {},
-        keys = {
-          gf = function(self)
-            local f = vim.fn.findfile(vim.fn.expand '<cfile>')
-            if f == '' then
-              Snacks.notify.warn 'No file under cursor'
-            else
-              self:close()
-              vim.cmd('e ' .. f)
-            end
-          end,
-          term_normal = {
-            '<esc>',
-            function(self)
-              self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
-              if self.esc_timer:is_active() then
-                self.esc_timer:stop()
-                vim.cmd 'stopinsert'
-              else
-                self.esc_timer:start(200, 0, function() end)
-                return '<esc>'
-              end
-            end,
-            mode = 't',
-            expr = true,
-            desc = 'Double escape to normal mode',
-          },
-        },
-      },
-    },
+    styles = {},
   },
   keys = {
     {
