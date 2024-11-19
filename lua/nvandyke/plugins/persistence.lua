4f8c92371e96225e8cafa3bbe54137c6a07e3fde
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-18 19:37:27 -0700

    Fix sessions sometimes being unavailable from dashboard

--- lua/nvandyke/plugins/persistence.lua
+++ lua/nvandyke/plugins/persistence.lua
@@ -1,6 +1,5 @@
 return {
   'folke/persistence.nvim',
-  event = 'BufReadPre', -- this will only start session saving when an actual file was opened
   config = function()
     require('persistence').setup()
 
