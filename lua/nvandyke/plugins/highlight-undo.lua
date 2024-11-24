5dfcd76bc6f5730839272bc25431eee0e0957631
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-23 18:39:32 -0700

    fix highlight-undo

--- lua/nvandyke/plugins/highlight-undo.lua
+++ lua/nvandyke/plugins/highlight-undo.lua
@@ -1,4 +1,28 @@
 return {
   'tzachar/highlight-undo.nvim',
-  opts = {},
+  event = 'VeryLazy',
+  config = function()
+    -- https://github.com/tzachar/highlight-undo.nvim/issues/28
+    require('highlight-undo').setup {
+      duration = 300,
+      keymaps = {
+        undo = {
+          desc = 'undo',
+          hlgroup = 'HighlightUndo',
+          mode = 'n',
+          lhs = 'u',
+          rhs = nil,
+          opts = {},
+        },
+        redo = {
+          desc = 'redo',
+          hlgroup = 'HighlightUndo',
+          mode = 'n',
+          lhs = '<C-r>',
+          rhs = nil,
+          opts = {},
+        },
+      },
+    }
+  end,
 }
