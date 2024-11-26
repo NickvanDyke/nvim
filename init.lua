c7da5a5585202ed249c3dc09309de958b5615d84
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-25 17:44:02 -0700

    disable some rtp plugins

--- init.lua
+++ init.lua
@@ -60,4 +60,18 @@ require('lazy').setup({
   change_detection = {
     notify = false,
   },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'gzip',
+        'matchit',
+        'matchparen',
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+    },
+  },
 })
