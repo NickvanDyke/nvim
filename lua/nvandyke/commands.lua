1a8a04ea8fb52f81182d6c6ea95bff24b8124d03
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-26 09:09:01 -0700

    replace auto-save plugin with autocmd

--- lua/nvandyke/commands.lua
+++ lua/nvandyke/commands.lua
@@ -19,3 +19,14 @@ vim.api.nvim_create_autocmd('CursorHold', {
   end,
 })
 
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
+  callback = function()
+    if #vim.api.nvim_buf_get_name(0) ~= 0 and vim.bo.buflisted then
+      vim.cmd 'silent w'
+
+      -- local time = os.date '%I:%M %p'
+
+      vim.notify('autosaved', vim.log.levels.INFO, { timeout = 500 })
+    end
+  end,
+})
