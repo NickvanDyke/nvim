b4af01d436a3a137406f7341fd944a2fea103512
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-26 12:40:16 -0700

    change flash keybinds again lol

--- lua/nvandyke/plugins/flash.lua
+++ lua/nvandyke/plugins/flash.lua
@@ -11,28 +11,21 @@ return {
       -- backdrop = false,
     },
     modes = {
-      char = {
-        -- Prefer using flash for everything. Prevents conflict with surround plugin.
-        -- enabled = false,
-      },
       search = {
         -- Just always integrate with search
         -- instead of having an entirely separate function.
         -- Mainly to keep s free for surround plugin.
         -- Can always just press enter to finish search normally;
         -- Seems like a win-win.
-        enabled = true,
+        -- enabled = true,
       },
     },
   },
   -- stylua: ignore
   keys = {
-    -- { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-
+    -- TODO: have to wait for timeoutlen when using with `y` because of nvim-surround `ys` mapping
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     {
       "R",
       mode = { "n", "x", "o" },
@@ -45,7 +38,7 @@ return {
       function() require("flash").remote() end,
       desc = "Remote Flash",
     },
-    '/',
+    -- '/',
     '?',
     'f',
     'F',
