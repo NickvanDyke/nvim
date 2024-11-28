9ab67750d6f5aa81b6103a20fb86ae9c61dd6cb0
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-28 08:34:04 -0600

    dont trigger autosave for not real bufs

--- lua/nvandyke/commands.lua
+++ lua/nvandyke/commands.lua
@@ -23,7 +23,7 @@ vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
   callback = function()
     local is_modified = vim.api.nvim_buf_get_option(0, 'modified')
     -- if #vim.api.nvim_buf_get_name(0) ~= 0 and vim.bo.buflisted then
-    if is_modified then
+    if is_modified and vim.bo.buflisted then
       vim.cmd 'silent w'
 
       -- local time = os.date '%I:%M %p'
