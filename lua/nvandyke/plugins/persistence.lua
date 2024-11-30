83183d6ff465cec14f15c6276f7bfa58e74b926a
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-30 13:24:54 -0600

    dont need that turns out

--- lua/nvandyke/plugins/persistence.lua
+++ lua/nvandyke/plugins/persistence.lua
@@ -1,20 +1,6 @@
 return {
   'folke/persistence.nvim',
-  dependencies = {
-    'otavioschwanck/arrow.nvim',
+  opts = {
+    need = 0,
   },
-  config = function()
-    require('persistence').setup {
-      need = 0,
-    }
-
-    vim.api.nvim_create_autocmd('User', {
-      desc = 'Load arrow files',
-      pattern = 'PersistenceLoadPost',
-      callback = function()
-        require('arrow.git').refresh_git_branch() -- only if separated_by_branch is true
-        require('arrow.persist').load_cache_file()
-      end,
-    })
-  end,
 }
