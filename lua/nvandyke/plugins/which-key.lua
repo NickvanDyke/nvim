7022280282b67d5d5262aa34423c20db1c7f5bb4
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-30 14:59:33 -0600

    hide which-key icons

--- lua/nvandyke/plugins/which-key.lua
+++ lua/nvandyke/plugins/which-key.lua
@@ -5,7 +5,7 @@ return { -- Useful plugin to show you pending keybinds.
     preset = 'helix',
     icons = {
       -- set icon mappings to true if you have a Nerd Font
-      mappings = vim.g.have_nerd_font,
+      mappings = false,-- vim.g.have_nerd_font,
       -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
       -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
       keys = vim.g.have_nerd_font and {} or {
