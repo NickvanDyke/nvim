496ddcd362aa7cfe6638518eff868eecd833cbe6
Nick van Dyke <nickjvandyke15@gmail.com>
2024-11-30 14:59:40 -0600

    fix match-up conflicting with TS-context

--- lua/nvandyke/plugins/match-up.lua
+++ lua/nvandyke/plugins/match-up.lua
@@ -3,6 +3,7 @@ return {
   event = 'BufReadPost',
   opts = {},
   init = function ()
-    vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+    -- vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+    -- Sometimes conflicts with treesitter-context
   end
 }
