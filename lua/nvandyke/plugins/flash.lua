e92ab8a3b6236083f90f0e36bc17d8bedff722fe
Nick van Dyke <nickjvandyke15@gmail.com>
2024-12-02 09:02:27 -0700

    Disable flash S in visual mode cuz nvim-surround

--- lua/nvandyke/plugins/flash.lua
+++ lua/nvandyke/plugins/flash.lua
@@ -25,9 +25,10 @@ return {
   keys = {
     -- TODO: have to wait for timeoutlen when using with `y` because of nvim-surround `ys` mapping
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    -- no 'x' mode cuz nvim-surround in visual mode
     {
       "S",
-      mode = { "n", "x", "o" },
+      mode = { "n", "o" },
       function() require("flash").treesitter() end,
       desc = "Flash Treesitter",
     },
