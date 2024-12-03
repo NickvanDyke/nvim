257e0ac920db236edb996ad4c4a39fd463ecf70e
Nick van Dyke <nickjvandyke15@gmail.com>
2024-12-03 12:56:29 -0700

    Add obsidian-bridge

---
+++ lua/nvandyke/plugins/obsidian-bridge.lua
@@ -0,0 +1,14 @@
+return {
+  'oflisback/obsidian-bridge.nvim',
+  dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+  lazy = true,
+  events = {
+    -- `ft` causes it to load for floating windows, like LSP hover
+    'BufRead *.md',
+    'BufWrite *.md',
+  },
+  opts = {
+    -- TODO: no luck with https server thus far
+    -- obsidian_server_address = 'https://127.0.0.1:27124/',
+  },
+}
