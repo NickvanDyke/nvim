8075c613459ed6dcffdc0c43fda425a34c743619
Nick van Dyke <nickjvandyke15@gmail.com>
2024-12-06 18:13:36 -0700

    back to flash search

--- lua/nvandyke/plugins/flash.lua
+++ lua/nvandyke/plugins/flash.lua
@@ -17,34 +17,34 @@ return {
         -- Mainly to keep s free for surround plugin.
         -- Can always just press enter to finish search normally;
         -- Seems like a win-win.
-        -- enabled = true,
+        enabled = true,
       },
     },
   },
   -- stylua: ignore
   keys = {
     -- TODO: have to wait for timeoutlen when using with `y` because of nvim-surround `ys` mapping
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     -- no 'x' mode cuz nvim-surround in visual mode
     {
-      "S",
-      mode = { "n", "o" },
+      "R",
+      mode = { "n", "x", "o" },
       function() require("flash").treesitter() end,
       desc = "Flash Treesitter",
     },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search({
-      remote_op = {
-        restore = true,
-        motion = true,
-      }
-    }) end, desc = "Treesitter Search" },
+    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search({
+    --   remote_op = {
+    --     restore = true,
+    --     motion = true,
+    --   }
+    -- }) end, desc = "Treesitter Search" },
     {
       "r",
       mode = "o",
       function() require("flash").remote() end,
       desc = "Remote Flash",
     },
-    -- '/',
+    '/',
     '?',
     'f',
     'F',
