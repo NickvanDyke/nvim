9dcd28a890f977f72277f7f44d284a23942707be
Nick van Dyke <nickjvandyke15@gmail.com>
2024-12-06 23:07:54 -0700

    meow

--- lua/nvandyke/plugins/inc-rename.lua
+++ lua/nvandyke/plugins/inc-rename.lua
@@ -1,9 +1,9 @@
 return {
   'smjonas/inc-rename.nvim',
   config = function()
-    require('inc_rename').setup()
+    require('inc_rename').setup({})
     vim.keymap.set('n', '<leader>cr', function()
       return ':IncRename ' .. vim.fn.expand '<cword>'
-    end, { expr = true })
+    end, { expr = true, desc = 'IncRename' })
   end,
 }
