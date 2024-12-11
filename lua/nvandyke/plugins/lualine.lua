beda4ec883b4eae42b4f8eed6a76a0bc8b62bda3
Nick van Dyke <nickjvandyke15@gmail.com>
2024-12-11 12:53:47 -0700

    mewow

--- lua/nvandyke/plugins/lualine.lua
+++ lua/nvandyke/plugins/lualine.lua
@@ -54,12 +54,12 @@ return {
             icon = '', -- Same as my powerline prompt
           },
           {
-            -- 'b:gitsigns_head', -- NOTE: based on open file I think. Thus disappears when e.g. lazygit is focused
-            'branch',
+            'b:gitsigns_head', -- NOTE: based on open file I think. Thus disappears when e.g. lazygit is focused
             icon = '',
             fmt = function(str)
-              if string.len(str) > 11 then
-                return string.sub(str, 1, 11) .. '…'
+              -- 8 is just enough to show the Jira ticket
+              if string.len(str) > 8 and vim.o.columns <= 80 then
+                return string.sub(str, 1, 8) .. '…'
               else
                 return str
               end
@@ -68,19 +68,29 @@ return {
           {
             'diff',
             source = diff_source,
-            cond = function()
-              return false
-            end,
+            -- cond = function()
+            --   return false
+            -- end,
           },
           'diagnostics',
         },
-        lualine_c = {
+        lualine_c = {},
+        lualine_x = {
           {
             'filename',
             path = 1,
-            fmt = function(str)
+            fmt = function(str, ctx)
               local parts = vim.split(str, '/')
               local filename = '%#Bold#' .. parts[#parts] .. '%*'
+
+              -- local filename_to_show = filename:match '^index%..+$'
+              --     -- prefix parent dir
+              --     and vim.fn.fnamemodify(vim.fn.fnamemodify(vim.api.nvim_buf_get_name(ctx.bufnr), ':h'), ':t') .. '/' .. filename
+              --   or filename
+              -- local gitsigns = vim.b.gitsigns_status_dict
+              -- if gitsigns and gitsigns.changed > 0 then
+              --   filename = '%#GitSignsChange#' .. filename .. '%*'
+              -- end
               if #parts == 1 then
                 return filename
               else
@@ -92,33 +102,24 @@ return {
           },
           { 'filetype', icon_only = true, padding = 0, separator = '' },
           {
-            'windows',
-            cond = function()
-              return false
+            function(ctx)
+              return require('arrow.statusline').text_for_statusline_with_icons(ctx.bufnr)
             end,
-            show_modified_status = false,
-            filetype_names = {
-              ['snacks_dashboard'] = 'Dashboard',
-            },
-            fmt = function(str, ctx)
-              local filename = str
-              local filename_to_show = filename:match '^index%..+$'
-                  -- prefix parent dir
-                  and vim.fn.fnamemodify(vim.fn.fnamemodify(vim.api.nvim_buf_get_name(ctx.bufnr), ':h'), ':t') .. '/' .. filename
-                or filename
-
-              -- Takes a long time to load and we'll never need on the Dashboard
-              local arrow_icon = package.loaded['arrow'] and require('arrow.statusline').text_for_statusline_with_icons(ctx.bufnr) or nil
-              return arrow_icon and (filename_to_show .. ' ' .. arrow_icon) or filename_to_show
-            end,
+            -- cond = function()
+            --   -- Takes a long time to load and we'll never need on the Dashboard
+            --   -- TODO: doesn't work. Maybe lualine doesn't allow this with function component
+            --   return package.loaded['arrow']
+            -- end,
           },
         },
-        lualine_x = {},
         lualine_y = {
           'fancy_macro',
           'fancy_searchcount',
           {
             'copilot',
+            cond = function()
+              return false
+            end,
             symbols = {
               -- The idle symbol has a right-space but the spinners don't
               spinners = { '⠋ ', '⠙ ', '⠹ ', '⠸ ', '⠼ ', '⠴ ', '⠦ ', '⠧ ', '⠇ ', '⠏ ' },
@@ -131,6 +132,7 @@ return {
     -- Awkward to create hl group at the right time
     vim.api.nvim_create_autocmd('BufReadPre', {
       callback = function()
+        -- TODO: bg color is slightly off
         vim.api.nvim_set_hl(0, 'Bold', { bold = true })
       end,
     })
