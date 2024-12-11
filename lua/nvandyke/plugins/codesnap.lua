0babf60866c15617ae692687eeb7f48470ed24fb
Nick van Dyke <nickjvandyke15@gmail.com>
2024-12-11 07:59:09 -0700

    add codesnap

---
+++ lua/nvandyke/plugins/codesnap.lua
@@ -0,0 +1,18 @@
+return {
+  'mistricky/codesnap.nvim',
+  build = 'make build_generator',
+  cmd = 'CodeSnap',
+  opts = {
+    has_breadcrumbs = true,
+    has_line_number = true,
+    bg_padding = 0,
+  },
+  keys = {
+    {
+      '<leader>S',
+      '<Esc><cmd>CodeSnap<cr>', -- https://github.com/mistricky/codesnap.nvim/issues/103#issuecomment-2155887498
+      mode = 'x',
+      desc = '[S]creenshot code',
+    },
+  },
+}
