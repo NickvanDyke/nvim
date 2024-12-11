3287ca7daba401ee30b81dfd1338e8d68fe78a5b
Nick van Dyke <nickjvandyke15@gmail.com>
2024-12-10 17:54:53 -0700

    filter portal by buffer

--- lua/nvandyke/plugins/portal.lua
+++ lua/nvandyke/plugins/portal.lua
@@ -1,9 +1,26 @@
+---Return a stateful search predicate which picks a single
+---result for each buffer in the given list
+---@return Portal.SearchPredicate
+local function different()
+    local found = {}
+
+    ---@param content Portal.Content
+    ---@return boolean
+    return function(content)
+        if vim.tbl_contains(found, content.buffer) then
+            return false
+        end
+        found[#found + 1] = content.buffer
+        return true
+    end
+end
+
 return {
   'cbochs/portal.nvim',
   opts = {
     window_options = {
       border = vim.g.border_default,
-      height = 5,
+      height = 3,
     },
   },
   cmd = 'Portal',
@@ -13,6 +30,7 @@ return {
       function()
         require('portal.builtin').jumplist {
           direction = 'backward',
+          filter = different()
         }
       end,
       desc = 'Portal',
@@ -23,6 +41,7 @@ return {
       function()
         require('portal.builtin').jumplist {
           direction = 'forward',
+          filter = different()
         }
       end,
       desc = 'Portal',
