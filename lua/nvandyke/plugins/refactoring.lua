42599fb9957e9e6d15e829cfa44811c95f46b7ba
Nick van Dyke <nickjvandyke15@gmail.com>
2024-12-14 09:26:06 -0700

    dont need those keybinds

--- lua/nvandyke/plugins/refactoring.lua
+++ lua/nvandyke/plugins/refactoring.lua
@@ -9,30 +9,6 @@ return {
       end,
       desc = '[R]efactor',
     },
-    {
-      mode = 'n',
-      '<leader>dp',
-      function()
-        require('refactoring').debug.printf { below = false }
-      end,
-      desc = '[d]ebug [p]rint',
-    },
-    {
-      mode = { 'x', 'n' },
-      '<leader>dv',
-      function()
-        require('refactoring').debug.print_var {}
-      end,
-      desc = '[d]ebug print [v]ar',
-    },
-    {
-      mode = 'n',
-      '<leader>dc',
-      function()
-        require('refactoring').debug.cleanup {}
-      end,
-      desc = '[d]ebug print [c]leanup',
-    },
   },
   dependencies = {
     'nvim-lua/plenary.nvim',
