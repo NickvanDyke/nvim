8ad5a3118016df1321e049f08f0ae2f4db0b381a
Nick van Dyke <nickjvandyke15@gmail.com>
2024-12-23 14:31:29 -0700

    replaced by snacks toggle

--- lua/nvandyke/plugins/lsp.lua
+++ lua/nvandyke/plugins/lsp.lua
@@ -63,16 +63,6 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-          -- The following code creates a keymap to toggle inlay hints in your
-          -- code, if the language server you are using supports them
-          --
-          -- This may be unwanted, since they displace some of your code
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[t]oggle Inlay [h]ints')
-          end
-
           map('<leader>cD', function()
             vim.diagnostic.setqflist { open = true }
           end, 'Workspace [D]iagnostics')
