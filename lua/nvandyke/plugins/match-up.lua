86352fd688d9cc3b1964ecef2c306b986295ddeb
Nick van Dyke <nickjvandyke15@gmail.com>
2024-12-26 14:12:55 -0600

    Fix match-up flashing statusline

--- lua/nvandyke/plugins/match-up.lua
+++ lua/nvandyke/plugins/match-up.lua
@@ -3,7 +3,6 @@ return {
   event = 'BufReadPost',
   opts = {},
   init = function ()
-    -- vim.g.matchup_matchparen_offscreen = { method = 'popup' }
-    -- Sometimes conflicts with treesitter-context
+    vim.g.matchup_matchparen_offscreen = { method = 'status_manual' }
   end
 }
