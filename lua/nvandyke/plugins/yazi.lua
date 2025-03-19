5eb81e5785f49aa15c3dfd7e9178a4410502b8e4
Nick van Dyke <nickjvandyke15@gmail.com>
2025-03-19 07:44:59 -0600

    use c-s for horizontal split in yazi

--- lua/nvandyke/plugins/yazi.lua
+++ lua/nvandyke/plugins/yazi.lua
@@ -1,4 +1,3 @@
----@type LazySpec
 return {
   'mikavilpas/yazi.nvim',
   keys = {
@@ -22,12 +21,19 @@ return {
       desc = 'Resume the last yazi session',
     },
   },
-  ---@type YaziConfig
   opts = {
     -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = true,
     keymaps = {
       show_help = '<f1>',
+      open_file_in_horizontal_split = '<c-s>',
+      grep_in_directory = false, -- attempts to use telescope
     },
   },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    -- vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
 }
