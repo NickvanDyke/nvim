b067a7300cbece222b53ea86f11147d545d45595
Nick van Dyke <nickjvandyke15@gmail.com>
2025-03-19 07:59:31 -0600

    leader f for format

--- lua/nvandyke/plugins/cellular-automata.lua
+++ lua/nvandyke/plugins/cellular-automata.lua
@@ -1,5 +1,6 @@
 return {
   'Eandrju/cellular-automaton.nvim',
+  enabled = false,
   keys = {
     { '<leader>fml', '<cmd>CellularAutomaton make_it_rain<CR>', desc = 'Make it rain' },
   },
--- lua/nvandyke/plugins/conform.lua
+++ lua/nvandyke/plugins/conform.lua
@@ -4,7 +4,7 @@ return { -- Formatting
   cmd = { 'ConformInfo' },
   keys = {
     {
-      '<leader>bf',
+      '<leader>f',
       function()
         require('conform').format { async = true, lsp_format = 'fallback' }
       end,
