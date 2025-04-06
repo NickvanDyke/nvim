a94e769c4f3be323bb13502d39f8860656f1a0c5
Nick van Dyke <nickjvandyke15@gmail.com>
2025-04-06 09:33:37 -0600

    simplify copilot config after update

--- lua/nvandyke/plugins/copilot.lua
+++ lua/nvandyke/plugins/copilot.lua
@@ -1,27 +1,16 @@
 return {
   'zbirenbaum/copilot.lua',
-  -- wtf lol, shows error every type when no internet
-  -- https://github.com/zbirenbaum/copilot.lua/issues/165
-  -- enabled = false,
   event = 'InsertEnter',
   opts = {
+    panel = {
+      enabled = false,
+    },
     suggestion = {
       auto_trigger = true,
       hide_during_completion = false,
       keymap = {
-        accept = false,
+        accept = '<Tab>',
       },
     },
   },
-  config = function(_, opts)
-    require('copilot').setup(opts)
-    -- https://github.com/zbirenbaum/copilot.lua/discussions/153
-    vim.keymap.set('i', '<Tab>', function()
-      if require('copilot.suggestion').is_visible() then
-        require('copilot.suggestion').accept()
-      else
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
-      end
-    end, { desc = 'Super Tab' })
-  end,
 }
