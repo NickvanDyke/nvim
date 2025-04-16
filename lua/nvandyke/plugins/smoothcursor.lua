462208d0bdd01acc43de460b9f1780cc1edca08d
Nick van Dyke <nickjvandyke15@gmail.com>
2025-04-16 13:21:25 -0600

    improve SmoothCursor colorscheming

--- lua/nvandyke/plugins/smoothcursor.lua
+++ lua/nvandyke/plugins/smoothcursor.lua
@@ -23,28 +23,21 @@ return {
       -- ['t'] = 'lualine_a_terminal',
     }
 
-    -- Depending on colorscheme, CursorLine will 'merge' the sign column bg color and CursorLine.
-    -- But in other colorschemes, they disagree.
-    local cursorline_bg = vim.api.nvim_get_hl_by_name('CursorLineNr', true).background
-    vim.api.nvim_set_hl(0, 'SmoothCursor', {
-      bg = cursorline_bg,
-    })
-
     local function sync_cursor_to_mode()
-      local mode = vim.fn.mode()
-      local mode_hl = mode_to_hl_name[mode] or 'lualine_a_normal'
-      local mode_color = vim.api.nvim_get_hl_by_name(mode_hl, true).background
+      local mode_hl = mode_to_hl_name[vim.fn.mode()] or 'lualine_a_normal'
+
+      local mode_color = vim.api.nvim_get_hl(0, { name = mode_hl }).bg
+      local cursorline_bg = vim.api.nvim_get_hl(0, { name = 'CursorLine' }).bg
 
       vim.api.nvim_set_hl(0, 'SmoothCursor', {
         fg = mode_color,
+        bg = cursorline_bg,
       })
     end
 
     sync_cursor_to_mode()
 
     vim.api.nvim_create_autocmd({ 'ModeChanged', 'ColorScheme' }, {
-      -- TODO: doesn't seem to trigger immediately for some mode changes.
-      -- Lualine sometimes does and sometimes doesn't.
       callback = sync_cursor_to_mode,
     })
   end,
