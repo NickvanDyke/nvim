c2c8ef1b16dfd042490cd9f32908658602724326
Nick van Dyke <nickjvandyke15@gmail.com>
2025-04-17 09:22:08 -0600

    add some whichkey groups

--- lua/nvandyke/plugins/which-key.lua
+++ lua/nvandyke/plugins/which-key.lua
@@ -50,6 +50,10 @@ return { -- Useful plugin to show you pending keybinds.
       { '<leader>d', group = 'Debug', mode = { 'n', 'v' } },
       { '<leader>u', group = 'User' },
       { '<leader>o', group = 'Orgmode' },
+      { 'gr', group = 'LSP' },
+      { 'gl', group = 'Log' },
+      { 'gL', group = 'Log w/ operator' },
+      { 'gp', group = 'LSP (preview)' },
     },
   },
 }
