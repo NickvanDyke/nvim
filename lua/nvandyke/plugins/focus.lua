cdd8a245393f8d923830e9f1f7922648ea235ac5
Nick van Dyke <nickjvandyke15@gmail.com>
2025-04-18 10:52:41 -0600

    fix focus making qf height too high

--- lua/nvandyke/plugins/focus.lua
+++ lua/nvandyke/plugins/focus.lua
@@ -20,4 +20,19 @@ return {
       desc = 'Split nicely',
     },
   },
+  init = function()
+    local ignore_filetypes = { 'qf' }
+    local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+    vim.api.nvim_create_autocmd('FileType', {
+      group = augroup,
+      callback = function(_)
+        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+          vim.b.focus_disable = true
+        else
+          vim.b.focus_disable = false
+        end
+      end,
+      desc = 'Disable focus autoresize for FileType',
+    })
+  end,
 }
