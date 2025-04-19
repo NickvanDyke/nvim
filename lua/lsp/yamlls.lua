f0adc3aa207e7806d06e1cdd1f890d3eb5089a35
Nick van Dyke <nickjvandyke15@gmail.com>
2025-04-19 08:48:29 -0600

    actually working native lsp config

--- lua/lsp/yamlls.lua
+++
@@ -1,9 +0,0 @@
-return {
-  settings = {
-    yaml = {
-      schemas = {
-        ['https://json.schemastore.org/circleciconfig.json'] = '/.circleci/config.yml',
-      },
-    },
-  },
-}
--- lua/nvandyke/plugins/lspconfig.lua
+++ lua/nvandyke/plugins/lspconfig.lua
@@ -1,37 +1,37 @@
 return {
   'neovim/nvim-lspconfig',
   dependencies = {
-    {
-      'williamboman/mason.nvim',
-      -- Call setup to add installed executables to PATH
-      opts = {},
-    },
-    {
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-      opts = {
-        ensure_installed = {
-          'lua-language-server',
-          'graphql-language-service-cli',
-          'terraform-ls',
-          'intelephense',
-          'css-lsp',
-          'vue-language-server',
-          'basedpyright',
-          'circleci-yaml-language-server',
-          'yaml-language-server',
-          'harper-ls',
-          'stylua',
-          'black',
-        },
-      },
-    },
-    {
-      'Massolari/lsp-auto-setup.nvim',
-      opts = {},
-    },
+    'williamboman/mason.nvim',
   },
   config = function()
-    -- Not included in lspconfig
-    vim.lsp.enable 'circleci-yaml-language-server'
+    -- Add installed executables to PATH
+    require('mason').setup()
+
+    -- Extend lspconfig defaults
+    vim.lsp.config('harper_ls', {
+      filetypes = { 'markdown' },
+    })
+    vim.lsp.config('yamlls', {
+      settings = {
+        yaml = {
+          schemas = {
+            ['https://json.schemastore.org/circleciconfig.json'] = '/.circleci/config.yml',
+          },
+        },
+      },
+    })
+
+    vim.lsp.enable {
+      'lua_ls',
+      'graphql',
+      'terraformls',
+      'intelephense',
+      'cssls',
+      'volar',
+      'basedpyright',
+      'yamlls',
+      'circleci',
+      'harper_ls',
+    }
   end,
 }
