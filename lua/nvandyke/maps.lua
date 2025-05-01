7d78ec8371c6ddfbc6aacb6e4fea84cb84d80eac
Nick van Dyke <nickjvandyke15@gmail.com>
2025-05-01 07:47:21 -0600

    Always paste to _ register

--- lua/nvandyke/maps.lua
+++ lua/nvandyke/maps.lua
@@ -7,7 +7,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 -- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 -- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 -- vim.keymap.set('n', '<C-q>', '<C-w><C-q>', { desc = 'Close current window' })
 
 vim.keymap.set({ 'n', 'v' }, 'H', '^', { desc = 'Move to the beginning of the line' })
@@ -21,7 +20,8 @@ vim.keymap.set('n', '<C-w><', '<cmd>vertical resize -7<cr>', { desc = 'Decrease 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selected lines down', silent = true })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selected lines up', silent = true })
 
-vim.keymap.set('x', 'P', '"_dP', { desc = 'Paste over selection without yanking' })
+-- This is almost always my preferred behavior
+vim.keymap.set('x', 'p', '"_dP', { desc = 'Paste over selection without yanking' })
 
 vim.keymap.set('n', 'y\\', function()
   -- -n means do not append newline
