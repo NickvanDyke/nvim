22dc07ab0fc051d8b9675790a493293e4818671b
Nick van Dyke <nickjvandyke15@gmail.com>
2025-05-06 09:23:19 -0600

    Extract snacks-dashboard

--- lua/nvandyke/plugins/snacks.lua
+++ lua/nvandyke/plugins/snacks.lua
@@ -4,17 +4,6 @@ return {
   lazy = false,
   opts = {
     -- explorer = {},
-    dashboard = {
-      enabled = true,
-
-      sections = {
-        { section = 'header' },
-        { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
-        { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
-        -- { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
-        { section = 'startup' },
-      },
-    },
     bigfile = { enabled = true },
     lazygit = {
       enabled = true,
