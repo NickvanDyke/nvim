44e72178402cca9031ead8247287ee9fae4539fd
Nick van Dyke <nickjvandyke15@gmail.com>
2025-05-06 09:21:53 -0600

    Extract snacks-scope

---
+++ lua/nvandyke/plugins/snacks-scope.lua
@@ -0,0 +1,31 @@
+return {
+  'folke/snacks.nvim',
+  opts = {
+    -- Indent/scope guide
+    indent = {
+      indent = {
+        enabled = false,
+      },
+    },
+    -- Scope-based movements and textobjects
+    scope = {
+      -- The textobjects ignore cursor column;
+      -- ignore it for the scope guide too so we know what we'll select
+      cursor = false,
+      filter = function()
+        -- It works weirdly in markdown
+        return vim.bo.filetype ~= 'markdown'
+      end,
+      keys = {
+        textobject = {
+          ii = {
+            linewise = true,
+          },
+          ai = {
+            linewise = true,
+          },
+        },
+      },
+    },
+  },
+}
--- lua/nvandyke/plugins/snacks.lua
+++ lua/nvandyke/plugins/snacks.lua
@@ -4,26 +4,6 @@ return {
   lazy = false,
   opts = {
     -- explorer = {},
-    scope = {
-      -- The textobjects ignore cursor column;
-      -- ignore it for the scope guide too so we know what we'll select
-      cursor = false,
-      filter = function()
-        -- It works weirdly in markdown
-        return vim.bo.filetype ~= 'markdown'
-      end,
-      keys = {
-        textobject = {
-          ii = {
-            linewise = true,
-          },
-          ai = {
-            linewise = true,
-          },
-        },
-      },
-    },
-    indent = {},
     dashboard = {
       enabled = true,
 
