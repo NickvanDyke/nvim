ce94f3436a8593730b529e01c4b6b49551e27748
Nick van Dyke <nickjvandyke15@gmail.com>
2025-05-09 15:37:14 -0600

    This feels more natural for splits?

--- lua/nvandyke/options.lua
+++ lua/nvandyke/options.lua
@@ -49,7 +49,7 @@ vim.opt.timeoutlen = 300
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-vim.opt.splitkeep = 'cursor'
+vim.opt.splitkeep = 'screen'
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
