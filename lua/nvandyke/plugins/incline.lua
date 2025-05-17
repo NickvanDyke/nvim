f63958b015b518c15e69a794740059a031bb8878
Nick van Dyke <nickjvandyke15@gmail.com>
2025-05-17 10:54:52 -0600

    incline

--- lua/nvandyke/plugins/incline.lua
+++ lua/nvandyke/plugins/incline.lua
@@ -1,6 +1,5 @@
 return {
   'b0o/incline.nvim',
-  enabled = false,
   event = 'WinNew',
   config = function()
     -- TODO: delayed appearance on first open.
@@ -13,23 +12,19 @@ return {
         margin = { horizontal = 0 },
       },
       hide = {
-        only_win = true,
+        -- only_win = true,
       },
-      -- render = function(props)
-      --   local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-      --   if filename == '' then
-      --     filename = '[No Name]'
-      --   end
-      --   local ft_icon, ft_color = devicons.get_icon_color(filename)
-      --   local modified = vim.bo[props.buf].modified
-      --
-      --   return {
-      --     ft_icon and { '', guifg = ft_color } or '',
-      --     ft_icon and { ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
-      --     not ft_icon and { '', guifg = '#44406e' } or '',
-      --     { ' ' .. filename .. ' ', gui = modified and 'bold,italic' or 'bold', guibg = '#44406e' },
-      --   }
-      -- end,
+      render = function(props)
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t') or '[No Name]'
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
+        local modified = vim.bo[props.buf].modified
+
+        return {
+          { '', guifg = ft_color or '#44406e' },
+          { ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) },
+          { ' ' .. filename .. ' ', gui = modified and 'bold,italic' or nil, guibg = '#44406e' },
+        }
+      end,
     }
   end,
 }
