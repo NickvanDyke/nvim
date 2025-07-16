b31b847119c7e315c8d044c72dca283773729c3d
Nick van Dyke <nickjvandyke15@gmail.com>
2025-07-15 19:31:08 -0600

    yay opencode.nvim testing

--- lua/nvandyke/plugins/opencode.lua
+++ lua/nvandyke/plugins/opencode.lua
@@ -4,41 +4,38 @@ return {
   dependencies = {
     'folke/snacks.nvim',
   },
+  ---@type opencode.Config
   opts = {
-    auto_insert = true, -- overriding from snacks-terminal
+    auto_focus = false,
+    auto_insert = true, -- overriding from my snacks-terminal opts
+    -- Example context integration
+    context = {
+      ---@param prompt string
+      ---@return string|nil
+      grapple = function(prompt)
+        if not prompt:match '@grapple' then
+          return nil
+        end
+
+        local paths = {}
+        for _, tag in ipairs(require('grapple').tags() or {}) do
+          table.insert(paths, tag.path)
+        end
+        return table.concat(paths, '\n')
+      end,
+    },
   },
+  -- stylua: ignore
   keys = {
-    {
-      '<leader>ot',
-      function()
-        require('opencode').toggle()
-      end,
-      desc = 'Toggle opencode',
-    },
-    {
-      '<leader>oa',
-      function()
-        require('opencode').ask()
-      end,
-      desc = 'Ask opencode',
-      mode = { 'n', 'v' },
-    },
-    -- Example re-usable prompts
-    {
-      '<leader>oe',
-      function()
-        require('opencode').ask 'Explain this code'
-      end,
-      desc = 'Explain selected code',
-      mode = 'v',
-    },
-    {
-      '<leader>oc',
-      function()
-        require('opencode').ask 'Critique @file for correctness and readability'
-      end,
-      desc = 'Critique current file',
-      mode = 'n',
-    },
+    -- Example keymaps
+    { '<leader>ot', function() require('opencode').toggle() end, desc = 'Toggle opencode', },
+    { '<leader>oa', function() require('opencode').ask() end, desc = 'Ask opencode', mode = { 'n', 'v' }, },
+    -- Example commands
+    { '<leader>on', function() require('opencode').command('/new') end, desc = 'New opencode session', },
+    -- Example prompts
+    { '<leader>oe', function() require('opencode').send('Explain this code') end, desc = 'Explain selected code', mode = 'v', },
+    { '<leader>oc', function() require('opencode').send('Critique @file for correctness and readability') end, desc = 'Critique current file', },
+    { '<leader>od', function() require('opencode').send('Fix these @diagnostics') end, desc = 'Fix current file errors', },
+    -- { '<leader>of', function() require('opencode').send('Fix the @diagnostics under @cursor') end, desc = 'Fix errors under cursor', },
   },
 }
