ad84e46612a8844a0eac104acf51f0e398eb4a5d
Nick van Dyke <nickjvandyke15@gmail.com>
2025-07-22 15:26:54 -0600

    replace pairs

--- lua/nvandyke/plugins/blink-pairs.lua
+++ lua/nvandyke/plugins/blink-pairs.lua
@@ -1,6 +1,6 @@
 return {
   'saghen/blink.pairs',
-  enabled = false,
+  enabled = true,
   version = '*', -- (recommended) only required with prebuilt binaries
 
   -- download prebuilt binaries from github releases
@@ -8,25 +8,5 @@ return {
 
   --- @module 'blink.pairs'
   --- @type blink.pairs.Config
-  opts = {
-    mappings = {
-      -- you can call require("blink.pairs.mappings").enable() and require("blink.pairs.mappings").disable() to enable/disable mappings at runtime
-      enabled = true,
-      -- see the defaults: https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua#L10
-      pairs = {},
-    },
-    highlights = {
-      enabled = true,
-      groups = {
-        'BlinkPairsOrange',
-        'BlinkPairsPurple',
-        'BlinkPairsBlue',
-      },
-      matchparen = {
-        enabled = true,
-        group = 'MatchParen',
-      },
-    },
-    debug = false,
-  },
+  opts = {},
 }
