33ec8e66b4cfc09a1ec5ff2212f74f6afb98c60a
Nick van Dyke <nickjvandyke15@gmail.com>
2025-07-29 12:01:36 -0600

    move mini.diff toggle overlay out of snacks.toggle

--- lua/nvandyke/plugins/mini-diff.lua
+++ lua/nvandyke/plugins/mini-diff.lua
@@ -1,6 +1,14 @@
 return {
   'echasnovski/mini.diff',
-  version = '*',
   event = 'BufReadPost',
   opts = {},
+  keys = {
+    {
+      '<leader>go',
+      function()
+        require('mini.diff').toggle_overlay(0)
+      end,
+      desc = 'Toggle Diff Overlay',
+    },
+  },
 }
