e960952d3afa52b0cbcaaa60c3d13440cb009dea
Nick van Dyke <nickjvandyke15@gmail.com>
2025-08-13 09:43:22 -0600

    extract snacks-git

--- lua/nvandyke/plugins/snacks.lua
+++ lua/nvandyke/plugins/snacks.lua
@@ -8,19 +8,6 @@ return {
       enabled = true,
     },
     bigfile = { enabled = true },
-    lazygit = {
-      enabled = true,
-      -- https://github.com/folke/snacks.nvim/issues/46
-      -- Change default settings to open edits in the current tab, not a new one
-      config = {
-        os = {
-          edit = '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})',
-          editAtLine = '[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" &&  nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")',
-          editAtLineAndWait = 'nvim +{{line}} {{filename}}',
-          openDirInEditor = '[ -z "$NVIM" ] && (nvim -- {{dir}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{dir}})',
-        },
-      },
-    },
     -- WARNING: Breaks and issues tons of warnings in VSCode Neovim
     words = { enabled = true },
     -- statuscolumn = {
@@ -90,44 +77,6 @@ return {
         Snacks.words.jump(-vim.v.count1)
       end,
       desc = 'Prev Reference',
-    },
-    {
-      '<leader>gg',
-      function()
-        Snacks.lazygit.open {
-          -- Current file's directory, in case Neovim's cwd is not a git repository (like the parent of many repos)
-          cwd = vim.fn.expand '%:h',
-        }
-      end,
-      desc = 'LazyGit',
-    },
-    {
-      '<leader>gf',
-      function()
-        Snacks.lazygit.log_file()
-      end,
-      desc = 'LazyGit Log (current file)',
-    },
-    {
-      '<leader>gl',
-      function()
-        Snacks.lazygit.log()
-      end,
-      desc = 'LazyGit Log',
-    },
-    {
-      '<leader>gb',
-      function()
-        Snacks.git.blame_line()
-      end,
-      desc = 'Git Blame Line',
-    },
-    {
-      '<leader>gB',
-      function()
-        Snacks.gitbrowse()
-      end,
-      desc = 'Git Browse',
     },
   },
 }
