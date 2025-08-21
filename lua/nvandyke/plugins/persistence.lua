2b5918d708394c5aaf21b3d2d0eabc44ba9bdcb1
Nick van Dyke <nickjvandyke15@gmail.com>
2025-08-21 09:34:37 -0600

    dont restore snacks windows that would be blank

--- lua/nvandyke/plugins/persistence.lua
+++ lua/nvandyke/plugins/persistence.lua
@@ -4,4 +4,10 @@ return {
   opts = {
     need = 0,
   },
+  config = function(_, opts)
+    require('persistence').setup(opts)
+
+    -- Remove 'blank' from the default setting to ignore snacks explorer, notifications history, etc. otherwise they restore empty
+    vim.o.sessionoptions = vim.o.sessionoptions:gsub('blank,', '')
+  end,
 }
