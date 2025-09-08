0e779237410aaec8afa5c3d48369809a7decf71d
Nick van Dyke <nickjvandyke15@gmail.com>
2025-09-08 09:45:47 -0600

    grapple tag by letter

--- lua/nvandyke/plugins/grapple.lua
+++ lua/nvandyke/plugins/grapple.lua
@@ -13,19 +13,56 @@ return {
   },
   keys = {
     {
+      'm',
+      function()
+        local char = vim.fn.getcharstr()
+        -- Handle ESC, Ctrl-C, etc.
+        if char == '' or vim.startswith(char, '<') then
+          return
+        end
+        local grapple = require 'grapple'
+        grapple.tag { name = char }
+        local filepath = vim.api.nvim_buf_get_name(0)
+        local filename = vim.fn.fnamemodify(filepath, ':t')
+        vim.notify('Marked ' .. filename .. ' as ' .. char, vim.log.levels.INFO, { title = 'Grapple' })
+      end,
+      noremap = true,
+      silent = true,
+      desc = 'Save grapple mark',
+    },
+    {
+      "'",
+      function()
+        local char = vim.fn.getcharstr()
+        -- Handle ESC, Ctrl-C, etc.
+        if char == '' or vim.startswith(char, '<') then
+          return
+        end
+        local grapple = require 'grapple'
+        if char == "'" then
+          grapple.toggle_tags()
+          return
+        end
+        grapple.select { name = char }
+      end,
+      noremap = true,
+      silent = true,
+      desc = 'Open grapple mark',
+    },
+    {
       '<C-g>',
       function()
         require('grapple').toggle()
       end,
       desc = 'Add file to grapple',
     },
-    {
-      '<C-e>',
-      function()
-        require('grapple').toggle_tags()
-      end,
-      desc = 'Toggle grapple menu',
-    },
+    -- {
+    --   '<C-e>',
+    --   function()
+    --     require('grapple').toggle_tags()
+    --   end,
+    --   desc = 'Toggle grapple menu',
+    -- },
     {
       '<C-1>',
       function()
